f2e3f165aecd5108c7516e6f3aa596267046fa8f
Nicolas <nicolas.bayle.it@gmail.com>
2019-02-02 07:53:56 +0000

    20190202: first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# aviBoostrap
+Prerequisites:
+- Make sure 1/3 Vm(s) has/have been deployed with the Avi software installed
+
+Input:
+- an inventory file with the following format (could be 1 or 3 controller hosts):
+#---
+#all:
+#  children:
+#    controller:
+#      hosts:
+#        192.168.17.179:
+
+Use the ansible playbook to
+- Generate a yaml file with all the information (to feed the python SDK)
+- Wait for the Avi portal to be up
+- Change the default password on the primary/first controller
+- Configure the Azure cloud (if avi.controller.cloud == azure and controller hosts == 3 and avi.controller.clusterIp)
+- Configure the cluster without the IP cluster (if controller hosts == 3 and not avi.controller.clusterIp)
+- Configure the cluster with IP cluster (if controller hosts == 3 and avi.controller.clusterIp)
+- Check the status of the controller
+- Display a message with all the information
+
+All the paramaters are stored in var/params.yml
+
+# avi:
+#   controller:
+#     newPassword: XXXXXX
+#     version: 17.2.14
+#     clusterIpStatus: true
+#     clusterIp: 172.16.1.10
+#     cloud: azure
+
+Example:
+ansible-playbook -i hosts main.yml
+
+All the variables are stored in vars/params.yml
+
+Script has been tested against:
+- Env: Ubuntu 16.04, Azure
+- Avi 17.2.14
+- Ansible 2.7.0
---
+++ generateYaml.py
@@ -0,0 +1,49 @@
+import requests, json, os, yaml, sys
+#
+# This python script reads an ansible host inventory file like the following:
+# ---
+# all:
+#   children:
+#     jump:
+#       hosts:
+#         172.16.1.4:
+#     controller:
+#       hosts:
+#         172.16.1.5:
+#         172.16.1.6:
+#         172.16.1.7:
+#   vars:
+#     ansible_user: "avi"
+#     ansible_ssh_private_key_file: "/home/avi/.ssh/id_rsa.azure"
+#
+# and creates a yml file like the following:
+#
+# avi_cluster:
+#   ip:
+#   - 172.16.1.5
+#   - 172.16.1.7
+#   name: avi-cluster
+# avi_credentials:
+#   api_version: 17.2.14
+#   controller: 172.16.1.6
+#   password: Avi_2019
+#   username: admin
+#
+hostFile = sys.argv[1]
+password = sys.argv[2]
+version = sys.argv[3]
+username = 'admin'
+with open(hostFile, 'r') as stream:
+    data_loaded = yaml.load(stream)
+stream.close
+controllerLeader = [*data_loaded['all']['children']['controller']['hosts']][0]
+if len([*data_loaded['all']['children']['controller']['hosts']]) == 1:
+  avi_credentials = { 'avi_credentials': {'controller' : controllerLeader, 'username': username, 'password': password, 'api_version': version}, 'avi_cluster': False}
+if len([*data_loaded['all']['children']['controller']['hosts']]) == 3:
+  controllerFollower = []
+  controllerFollower.append([*data_loaded['all']['children']['controller']['hosts']][1])
+  controllerFollower.append([*data_loaded['all']['children']['controller']['hosts']][2])
+  avi_credentials = { 'avi_credentials': {'controller' : controllerLeader, 'username': username, 'password': password, 'api_version': version}, 'avi_cluster': {'name': 'avi-cluster', 'ip': controllerFollower }}
+with open('vars/creds.yml', 'w') as outfile:
+    yaml.dump(avi_credentials, outfile, default_flow_style=False)
+outfile.close
