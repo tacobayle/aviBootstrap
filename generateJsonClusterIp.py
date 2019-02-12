32bf1bba53c92ea26b04a6c961ea23c3b59b6f93
Nicolas Bayle <nicolas.bayle.it@gmail.com>
2019-02-12 15:11:25 +0000

    20190212: updated

--- generateJsonClusterIp.py
+++ generateJsonClusterIp.py
@@ -6,8 +6,8 @@ username = 'admin'
 with open(paramsFile, 'r') as stream:
     data_loaded = yaml.load(stream)
 stream.close
-clusterIP = data_loaded['avi']['controller']['clusterIp']
-avi_credentials = { 'avi_credentials': {'controller' : clusterIP, 'username': username, 'password': password, 'api_version': version}}
+clusterIP = data_loaded['controller']['clusterIp']
+avi_credentials = {'controller' : clusterIP, 'username': username, 'password': password, 'api_version': version}
 # with open('vars/creds.yml', 'w') as outfile:
 #     yaml.dump(avi_credentials, outfile, default_flow_style=False)
 # outfile.close
